f33813c85dc0425087dceadad2c6e1a8790acc6e
Poobesh <poobesh1999@gmail.com>
2020-04-11 13:28:13 +0530

    After_Frontend
    Fully Finished Front End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,22 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Expense Calculator";
-  expenses :string ='';
-  
+  expense: number = 0;
+  expenses: Array<number>;
+  income: Array<number>;
+
+  onClear() {}
+  onAddAmount(data) {
+    let x = Number(data.value);
+    this.expense += x;
+    if (x > 0) {
+      this.income.push(x);
+    } else {
+      if (x < 0) {
+        this.expenses.push(x);
+      } else {
+        console.log("ERROR");
+      }
+    }
+  }
 }
